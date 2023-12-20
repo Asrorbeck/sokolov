a4fd9c8e53dbb1e57cc892f88ce4051bee57f4e6
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2023-12-20 23:45:00 +0500

    1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,59 @@
+const musicControl = document.getElementById("navbar__btn");
+const audio = document.getElementById("background-music");
+const musicIcon = document.getElementById("music-icon");
+
+let isPlaying = true;
+
+var modal = document.getElementById("myModal");
+var yesBtn = document.getElementById("yesBtn");
+var noBtn = document.getElementById("noBtn");
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+yesBtn.addEventListener("click", function () {
+  audio.muted = false
+  audio.play();
+  musicIcon.src = "./img/unmute.svg";
+  closeModal();
+
+  isPlaying = !isPlaying;
+});
+
+noBtn.addEventListener("click", function () {
+  audio.pause();
+  musicIcon.src = "./img/mute.svg";
+  closeModal();
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+window.onload = function () {
+  modal.style.display = "block";
+}
+
+musicControl.addEventListener("click", function () {
+  if (isPlaying) {
+    audio.muted = false
+    audio.play();
+    musicIcon.src = "./img/unmute.svg";
+  } else {
+    audio.pause();
+    musicIcon.src = "./img/mute.svg";
+  }
+
+  isPlaying = !isPlaying;
+});
+
+
+
+
+
 const targetDate = new Date("2023-12-23T12:00:00").getTime();
 
 const countdown = setInterval(function () {
@@ -19,3 +75,12 @@ const countdown = setInterval(function () {
     document.getElementById("seconds").innerText = seconds;
   }
 }, 1000);
+
+
+
+function toggleMenu() {
+  var menu = document.querySelector('.menu');
+  var overlay = document.querySelector('.menu-overlay');
+  menu.classList.toggle('menu-open');
+  overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
+}
