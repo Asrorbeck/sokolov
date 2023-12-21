ed50329ddfb65532522e46d43f5381dc3ec9a4cb
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2023-12-21 08:29:07 +0500

    1

--- js/index.js
+++ js/index.js
@@ -86,13 +86,14 @@ function toggleMenu() {
 }
 
 document.getElementById("openMaps").addEventListener("click", function () {
-  const address = "ТРЦ Riviera";
+  const latitude = 41.339703; // Replace with the actual latitude
+  const longitude = 69.254248; // Replace with the actual longitude
 
   if (/iphone|ipad|ipod/i.test(navigator.userAgent)) {
     // Open Yandex Maps on iOS
-    window.location.href = `https://maps.yandex.com/?text=${encodeURIComponent(address)}`;
+    window.location.href = `https://maps.yandex.com/?ll=${longitude},${latitude}`;
   } else if (/android/i.test(navigator.userAgent)) {
     // Open Yandex Navigator on Android
-    window.location.href = `https://yandex.com/maps/?text=${encodeURIComponent(address)}`;
+    window.location.href = `https://yandex.com/maps/?ll=${longitude},${latitude}`;
   }
 });
